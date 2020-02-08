c7e2ee567d4f3f411eda1485bdbda5527f7c2f46
IVIanuu <ivianuu@gmail.com>
2020-02-08 13:14:09 +0100

    ...

--- vector_xml_to_kt.kts
+++ vector_xml_to_kt.kts
@@ -1,17 +1,37 @@
 #!/usr/bin/env kscript
+@file:MavenRepository("ivianuu", "https://dl.bintray.com/ivianuu/maven/")
 @file:MavenRepository("jcenter", "https://jcenter.bintray.com/")
 @file:DependsOnMaven("com.squareup:kotlinpoet:1.5.0")
-@file:Include("Icon.kt")
-@file:Include("IconParser.kt")
-@file:Include("PathNode.kt")
-@file:Include("PathParser.kt")
-@file:Include("VectorAssetGenerator.kt")
-@file:Include("VectorNode.kt")
+@file:DependsOnMaven("com.ivianuu.vectorconverter:vector-converter:0.0.1-dev1")
+@file:DependsOnMaven("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.61")
+@file:DependsOnMaven("com.google.guava:guava:28.2-jre")
 
-val inputFile = args.getOrNull(0) ?: error("missing input file")
-check(inputFile.endsWith(".xml"))
+import com.google.common.base.CaseFormat
+import com.ivianuu.vectorconverter.xmlToKotlin
+import java.io.File
+import java.util.regex.Pattern
 
-val outputFile = args.getOrNull(1) ?: error("missing output file")
-check(inputFile.endsWith(".kt"))
+val inputFilePath = args.getOrNull(0) ?: error("missing input file")
+check(inputFilePath.endsWith(".xml"))
 
-error("hello i $inputFile o $outputFile")
+val inputFile = File(inputFilePath)
+check(inputFile.exists())
+
+val userDir = System.getProperty("user.dir")
+
+val iconName = inputFile.name
+        .removePrefix("ic_")
+        .removeSuffix(".xml")
+        .let { CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, it) }
+
+val xmlIcon = inputFile.readText()
+
+println("Convert $inputFilePath\n\n$xmlIcon")
+
+val kotlinIcon = xmlToKotlin(
+        xml = xmlIcon,
+        packageName = "com.ivianuu.no.packagename",
+        iconName = iconName
+)
+
+println("Created kotlin file\n\n$kotlinIcon")
